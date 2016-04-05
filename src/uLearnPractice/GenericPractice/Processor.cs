8f01ccfac60356a507ed8fe476fd47f5885090e6
poma.prs <poma.prs@gmail.com>
2016-04-05 19:12:00 +0400

    add generic practice solution

---
+++ src/uLearnPractice/GenericPractice/Processor.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApplication1
+{
+    public interface IProcessor
+    {
+        IProcessor<TEngine> Create<TEngine>();
+    }
+
+    public interface IProcessor<TEngine, TEntity>
+    {
+        Processor<TEngine, TEntity, TLogger> With<TLogger>();
+    }
+
+    public interface IProcessor<TEngine>
+    {
+        IProcessor<TEngine, TEntity> For<TEntity>();
+    }
+
+    public class Processor : IProcessor
+    {
+        public IProcessor<TEngine> Create<TEngine>()
+        {
+            return new Processor<TEngine>();
+        }
+
+        public static IProcessor<TEngine> CreateEngine<TEngine>()
+        {
+            return new Processor().Create<TEngine>();
+        }
+    }
+
+    public class Processor<TEngine> : IProcessor<TEngine>
+    {
+        public IProcessor<TEngine, TEntity> For<TEntity>()
+        {
+            return new Processor<TEngine, TEntity>();
+        }
+    }
+
+    public class Processor<TEngine, TEntity> : IProcessor<TEngine, TEntity>
+    {
+        public Processor<TEngine, TEntity, TLogger> With<TLogger>()
+        {
+            return new Processor<TEngine, TEntity, TLogger>();
+        }
+    }
+
+    public class Processor<TEngine, TEntity, TLogger> { }
+}
---
+++ src/uLearnPractice/GenericPractice/Storage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    public interface IStorage<TKey>
+    {
+        T Create<T>() where T : new();
+        IDictionary<TKey, T> GetWith<T>();
+        T Get<T>(TKey guid);
+    }
+
+    public class GuidStorage : IStorage<Guid>
+    {
+        private Dictionary<Guid, object> Storage { get; } = new Dictionary<Guid, object>();
+
+        public T Create<T>() where T : new()
+        {
+            var entity = new T();
+            var guid = new Guid();
+            Storage[guid] = entity;
+            return entity;
+        }
+
+        public IDictionary<Guid, T> GetWith<T>()
+        {
+            return Storage
+                .Where(x => x.Value is T)
+                .ToDictionary(x => x.Key, x => (T) x.Value);
+        }
+
+        public T Get<T>(Guid guid)
+        {
+            if (!Storage.ContainsKey(guid))
+                return default(T);
+            var entity = Storage[guid];
+            if (entity is T)
+                return (T) entity;
+            return default(T);
+        }
+    }
+}
