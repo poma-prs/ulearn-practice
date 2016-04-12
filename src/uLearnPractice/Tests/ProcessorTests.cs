83558ee03a353ab7ea953639bd03e3943988930e
poma.prs <poma.prs@gmail.com>
2016-04-12 12:58:24 +0400

    add test project
    add generic tests
    add processor tests

---
+++ src/uLearnPractice/Tests/ProcessorTests.cs
@@ -0,0 +1,16 @@
+﻿using GenericPractice;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ProcessorTests
+    {
+        [Test]
+        public void CheckCreatedType_Test()
+        {
+            var processor = Processor.CreateEngine<int>().For<string>().With<long>();
+            Assert.AreEqual(typeof(Processor<int, string, long>), processor.GetType());
+        }
+    }
+}
---
+++ src/uLearnPractice/Tests/StorageTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Linq;
+using GenericPractice;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class StorageTests
+    {
+        private IStorage<Guid> Storage { get; set; }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            Storage = new GuidStorage();
+            Storage.Create<int>();
+            Storage.Create<Guid>();
+            Storage.Create<StorageTests>();
+            Storage.Create<GuidStorage>();
+            Storage.Create<GuidStorage>();
+            Storage.Create<int>();
+        }
+
+        [Test]
+        public void GetWithIntCount_Test()
+        {
+            var items = Storage.GetWith<int>();
+            Assert.AreEqual(2, items.Count);
+        }
+
+        [Test]
+        public void GetWithGuidStorageCount_Test()
+        {
+            var items = Storage.GetWith<GuidStorage>();
+            Assert.AreEqual(2, items.Count);
+        }
+
+        [Test]
+        public void GetWithStorageTestsCount_Test()
+        {
+            var items = Storage.GetWith<StorageTests>();
+            Assert.AreEqual(1, items.Count);
+        }
+
+        [Test]
+        public void GetWithGuidCount_Test()
+        {
+            var items = Storage.GetWith<Guid>();
+            Assert.AreEqual(1, items.Count);
+        }
+
+        [Test]
+        public void GetInt_Test()
+        {
+            var items = Storage.GetWith<int>();
+            var all = items.All(x =>
+            {
+                var item = Storage.Get<int>(x.Key);
+                return item.Equals(x.Value);
+            });
+            Assert.IsTrue(all);
+        }
+
+        [Test]
+        public void GetGuid_Test()
+        {
+            var items = Storage.GetWith<Guid>();
+            var all = items.All(x =>
+            {
+                var item = Storage.Get<Guid>(x.Key);
+                return item.Equals(x.Value);
+            });
+            Assert.IsTrue(all);
+        }
+
+        [Test]
+        public void GetGuidStorage_Test()
+        {
+            var items = Storage.GetWith<GuidStorage>();
+            var all = items.All(x =>
+            {
+                var item = Storage.Get<GuidStorage>(x.Key);
+                return item.Equals(x.Value);
+            });
+            Assert.IsTrue(all);
+        }
+
+        [Test]
+        public void GetStorageTests_Test()
+        {
+            var items = Storage.GetWith<StorageTests>();
+            var all = items.All(x =>
+            {
+                var item = Storage.Get<StorageTests>(x.Key);
+                return item.Equals(x.Value);
+            });
+            Assert.IsTrue(all);
+        }
+    }
+}
